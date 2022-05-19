a6aa66cbb2528795a33f1f0a92f10491122cceff
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-05-18 20:42:30 -0700

    Header work

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 
+
 function Header() {
 	return (
-		<div>
-			<h1>Hello wold what is up</h1>
-			<img src="/assets/images/codysbee2.png" height="25" width="25" alt="A picture of a bee"/>
-
+		<div className="container">
+			<div className="row">
+				<img src="./assests\images/codybee2.png" height="100" width="100" alt="A picture of a bee" />
+				<h1>Cody's Busy Bees</h1>
+			</div>
 		</div>
 	);
 }
