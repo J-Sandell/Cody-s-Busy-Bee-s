4a0ec14d935f00ccc8f307a610a26ef285a6a780
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-05-20 20:34:12 -0700

    working on the header and nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/HeaderComponent';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,14 +1,22 @@
-import { Component } from "react";
+import React, { Component, Fragment } from "react";
+
+
 
 
 function Header() {
 	return (
+
 		<div className="container">
 			<div className="row">
-				<img src="./assests\images/codybee2.png" height="100" width="100" alt="A picture of a bee" />
-				<h1>Cody's Busy Bees</h1>
+				<div className="col">
+					<img src="./assests\images/codybee2.png" height="100" width="100" alt="A picture of a bee" />
+				</div>
+				<div className="col">
+					<h1>Cody's Busy Bees</h1>
+				</div>
 			</div>
 		</div>
+
 	);
 }
 
