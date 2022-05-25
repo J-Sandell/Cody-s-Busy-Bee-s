bbcb3ac82c6fa60c440f6a82b4596d7cead97f68
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-05-24 20:39:00 -0700

    Building header

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
-import { Nav } from "reactstrap";
+import { Navbar, Nav, NavLink, NavItem } from 'reactstrap';
+
 
 
 
@@ -8,21 +9,37 @@ import { Nav } from "reactstrap";
 function Header() {
 	return (
 		<Fragment>
-			<div className="container fixed-top">
+			<div className="container-fluid fixed-top">
 				<div className="row">
-					<div className="col fixed-top">
-						<img src="/assests\images/codybee2.png" height="100" width="100" alt="bee" />
-					</div>
-					<div>
-						<h1>Cody's</h1>
-						<h2>Busy Bee's</h2>
+					<div className="col text-light bg-dark">
+						<div className="d-flex shadow">
+							<img src="/assests\images/codybee2.png" height="100" width="100" alt="bee" />
+							<h1>Cody's</h1>
+							<h2>Busy Bee's</h2>
+						</div>
+						<div>
+							<Navbar>
+								<Nav>
+									<NavItem>
+										<NavLink href="./HomeComponent">Home</NavLink>
+									</NavItem>
+									<NavItem>
+										<NavLink href="./AboutComponent.js">Missons</NavLink>
+									</NavItem>
+									<NavItem>
+										<NavLink href="#">About</NavLink>
+									</NavItem>
+									<NavItem>
+										<NavLink href="#">Contact</NavLink>
+									</NavItem>
+								</Nav>
+							</Navbar>
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<Nav dark shadow sticky="top" >
 
-			</Nav>
 		</Fragment>
 	);
 }
